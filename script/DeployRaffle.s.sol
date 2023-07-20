f9c73e45df319895aa35713ed1c83928ba8c3fb2
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-07-20 11:31:29 +0530

    FUN-5 feat: create lock functionality

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -6,7 +6,55 @@ import {HelperConfigRaffle} from "./HelperConfigRaffle.s.sol";
 import {Raffle} from "../src/Raffle.sol";
 
 contract DeployRaffle is Script {
-    function run() public returns (Raffle, HelperConfigRaffle) {
-        
+
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        AddConsumer addConsumer = new AddConsumer();
+        (
+            uint64 subscriptionId,
+            bytes32 gasLane,
+            uint256 automationUpdateInterval,
+            uint256 raffleEntranceFee,
+            uint32 callbackGasLimit,
+            address vrfCoordinatorV2,
+            address link,
+            uint256 deployerKey
+        ) = helperConfig.activeNetworkConfig();
+
+        if (subscriptionId == 0) {
+            CreateSubscription createSubscription = new CreateSubscription();
+            subscriptionId = createSubscription.createSubscription(
+                vrfCoordinatorV2,
+                deployerKey
+            );
+
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinatorV2,
+                subscriptionId,
+                link,
+                deployerKey
+            );
+        }
+
+        vm.startBroadcast(deployerKey);
+        Raffle raffle = new Raffle(
+            subscriptionId,
+            gasLane,
+            automationUpdateInterval,
+            raffleEntranceFee,
+            callbackGasLimit,
+            vrfCoordinatorV2
+        );
+        vm.stopBroadcast();
+
+        // We already have a broadcast in here
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinatorV2,
+            subscriptionId,
+            deployerKey
+        );
+        return (raffle, helperConfig);
     }
 }
